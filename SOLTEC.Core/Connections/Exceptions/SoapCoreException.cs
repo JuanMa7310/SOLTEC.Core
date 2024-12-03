ae6574b586f67d183dd5a29b0ef5322ddfc89d4c
SOLTEC <61347205+JuanMa7310@users.noreply.github.com>
2024-12-03 17:12:08 -0600

    Add the Exception class for the SOAP connection.

---
+++ SOLTEC.Core/Connections/Exceptions/SoapCoreException.cs
@@ -0,0 +1,20 @@
+﻿using SOLTEC.Core.Exceptions;
+using System.Net;
+
+namespace SOLTEC.Core.Connections.Exceptions;
+
+public class SoapCoreException : ResultException 
+{
+    public SoapCoreException(string key, string reason, HttpStatusCode httpStatusCode, string errorMessage = "") {
+        Key = key;
+        Reason = reason;
+        ErrorMessage = errorMessage;
+        HttpStatusCode = httpStatusCode;
+    }
+}
+
+public enum SoapCoreErrorEnum 
+{
+    BadRequest = 400,
+    Deserialization = 401
+}
