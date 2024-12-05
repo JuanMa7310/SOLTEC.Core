d4f286250e0dc3368c745891740de8cf74fd79d6
SOLTEC <61347205+JuanMa7310@users.noreply.github.com>
2024-12-04 19:20:11 -0600

    Generate the Exception class for MQ messages.

---
+++ SOLTEC.Core/Exceptions/MessageMQException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using SOLTEC.Core.Enums;
+
+namespace SOLTEC.Core.Exceptions;
+
+public class MessageMQException : ResultException
+{
+    public MessageMQException(EventMessageErrorEnum reason, HttpStatusCode httpStatusCode = HttpStatusCode.BadRequest, 
+        string errorMessage = "")
+    {
+        Key = "MessageMQException";
+        HttpStatusCode = httpStatusCode;
+        Reason = reason.ToString();
+        ErrorMessage = errorMessage;
+    }
+}
+
